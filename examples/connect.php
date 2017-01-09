d9d9df54393fa45c1fe6fbd0f616848741c7cf5f
Maksym Tymchyk <reilag69@gmail.com>
2017-01-10 00:25:48 +0200

    Update Connect Example and Readme.MD dile

--- examples/connect.php
+++ examples/connect.php
@@ -2,45 +2,12 @@
 
 require_once dirname(__FILE__) . '/../vendor/autoload.php';
 
-use Three9Dice\Client;
-use Three9Dice\User;
-use Three9Dice\Bet\Bet;
-
-
 $apiKey     = 'Your API key';
 $username   = 'Your login';
 $password   = 'Your password';
 $totp       = 'Your Totp';
 
 
-$startTime = microtime(true);
-$three9DiceClient = new Client(
-	new User($apiKey, $username, $password)
+$three9DiceClient = new \Three9Dice\Client(
+	new \Three9Dice\User($apiKey, $username, $password, $totp)
 );
-echo "\n\n " . (microtime(true) - $startTime) . "\n\n";
-
-
-$baseBetAmount = 1;
-$bet = new Bet(
-	$baseBetAmount,
-	\Three9Dice\Constant::CURRENCY_ETH,
-	\Three9Dice\GuessRange\GuessRange::generateRandom(49.95)
-);
-
-while( true )
-{
-	$result = $three9DiceClient->placeBet($bet);
-
-	echo sprintf( "%s | %s \n", $result['BetId'], $result['PayOut'] );
-
-	if( $result['PayOut'] == 0 )
-	{
-		$bet->setAmount( $bet->getAmount() * 2 );
-	}
-	else
-	{
-		$bet->setAmount($baseBetAmount);
-	}
-	usleep(10);
-}
-
--- readme.md
+++ readme.md
@@ -1,3 +1,25 @@
 # Hello, Friends!
 
 It's initial version of [999dice.com](https://www.999dice.com/?79432757) PHP library!
+
+
+## Examples:
+
+### Connect:
+```php
+
+<?php
+
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
+
+$apiKey     = '<Your API key>';
+$username   = '<Your login>';
+$password   = '<Your password>';
+$totp       = '<Your Totp>';
+
+
+$three9DiceClient = new \Three9Dice\Client(
+	new \Three9Dice\User($apiKey, $username, $password, $totp)
+);
+
+```
